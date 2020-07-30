7e8a97d3886c73a1b9d685956e92746067d22f55
Shakir <shakir.azimli@adif.az>
2020-07-30 09:59:30 +0400

    Arrays

--- Hackerrank/30 Days of Code/src/Day_6/Let_s_Review.java
+++ Hackerrank/30 Days of Code/src/Day_6/Let_s_Review.java
@@ -1,4 +1,33 @@
 package Day_6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Let_s_Review {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int T=sc.nextInt();
+
+        for(int i=0;i<T;i++){
+            String a=sc.next();
+            for(int j =0;j<a.length();j++){
+                if(j%2==0)
+                    System.out.print(a.charAt(j));
+            }
+            System.out.print(" ");
+            for(int j=0;j<a.length();j++){
+                if(j%2==1)
+                    System.out.print(a.charAt(j));
+            }
+            System.out.println();
+        }
+
+        }
+
+
+
+
+
 }
---
+++ Hackerrank/30 Days of Code/src/Day_7/Arrays.java
@@ -0,0 +1,7 @@
+package Day_7;
+
+public class Arrays {
+    public static void main(String[] args) {
+        
+    }
+}
